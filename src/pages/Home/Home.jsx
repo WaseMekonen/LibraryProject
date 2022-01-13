02c2edae10318f446ba4ed09f34ba3cf4c6ced52
MekonenWase <mekonen.wase@gmail.com>
2022-01-13 19:59:13 +0200

    V6

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,22 @@
 import { Link } from "react-router-dom";
-
+import "./Home.css";
 export default function Home() {
   return (
     <>
-      <h2>Welcome</h2>
-      <button>
-        <Link to="/Login"> Login </Link>
-      </button>
-      <button>
-        <Link to="/Register"> Register </Link>
-      </button>
-      
+      <div className="banner">
+        <div className="">
+          <h2>Welcome </h2>
+          <h4>Login To Discover The Secret Of Library</h4>
+        </div>
+      </div>
+      <div className="buttons-container">
+        <div className="login-link">
+          <Link to="/Login"> Login </Link>
+        </div>
+        <div className="register-link">
+          <Link to="/Register"> Register </Link>
+        </div>
+      </div>
     </>
   );
 }
